8288aa4e10873a88a44fb39d679b157853ceaf53
Christopher Motes <motescj@aim.com>
2016-07-03 17:25:20 -0500

    fixed script problems.

--- publichttp.sh
+++ publichttp.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 yum -y update
 yum -y install httpd
-chkconfig on httpd
+chkconfig httpd on
 perl -pe 's/Amazon/Motes Public/g' /var/www/error/noindex.html > /var/www/html/index.html
 service httpd start
