07b5c89bc7ff5241e869bf77551616b3cd24529d
Motes, C.J <motescj@aim.com>
2016-07-03 20:39:04 -0500

    fixed privte data

--- privatehttp.sh
+++ privatehttp.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 yum -y update
 yum -y install httpd
-chkconfig on httpd
+chkconfig httpd on 
 perl -pe 's/Amazon/Motes Private/g' /var/www/error/noindex.html > /var/www/html/index.html
 service httpd start
